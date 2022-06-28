c2bde2c346696e2fab954a17f8cdc919069d3007
kaushik2107-bit <kaushiksbhowmick@gmail.com>
2022-06-28 20:52:14 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function toggle() {
+    var x = document.getElementById("menu-list");
+    if (x.style.display ==  "block") {
+        x.style.display = "none"; 
+    } else {
+        x.style.display = "block";
+    }
+    var y = document.getElementById("fa-times");
+    var z = document.getElementById("fa-bars");
+    if (y.style.display ==  "block") {
+        y.style.display = "none"; 
+        z.style.display = "block";
+        
+    } else {
+        z.style.display = "none";
+        y.style.display = "block";
+        
+    }   
+}
+function toggles() {
+    var x = document.getElementById("menu-list");
+    if (x.style.display ==  "block") {
+        x.style.display = "none"; 
+    } else {
+        x.style.display = "block";
+    }
+    var y = document.getElementById("fa-times");
+    var z = document.getElementById("fa-bars");
+    if (y.style.display ==  "block") {
+        y.style.display = "none"; 
+        z.style.display = "block";
+        
+    } else {
+        z.style.display = "none";
+        y.style.display = "block";
+        
+    }   
+}
+// document.addEventListener('mouseup', function(e) {
+//     var container = document.getElementById('menu-list');
+//     if (!container.contains(e.target)) {
+//         container.style.display = 'none';
+//     }
+//     var y = document.getElementById("fa-times");
+//     var z = document.getElementById("fa-bars");
+
+    
+//     y.style.display = "none"; 
+//     z.style.display = "block";
+// });
